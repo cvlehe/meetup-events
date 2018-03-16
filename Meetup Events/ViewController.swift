6997dbbb4f85c999714eda3cdf462f32ba753af6
Charles von Lehe <vonlehec@gmail.com>
2018-03-15 23:46:43 -0400

    Initial commit

---
+++ Meetup Events/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meetup Events
+//
+//  Created by Charles von Lehe on 3/15/18.
+//  Copyright © 2018 Charles von Lehe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
